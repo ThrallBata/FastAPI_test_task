eb1eeb794d968b239ce8412e0e38c817f2477b4f
ThrallBata <vicortobdel@gmail.com>
2023-06-09 10:16:04 +0700

    test

--- main.py
+++ main.py
@@ -5,3 +5,4 @@ import settings
 
 if __name__ == "__main__":
     uvicorn.run("app.api:app", host="0.0.0.0", port=settings.APP_PORT, reload=True)
+
--- settings.py
+++ settings.py
@@ -18,3 +18,4 @@ APP_PORT = env.int("APP_PORT", default="8080")
 SECRET_KEY = env.str("SECRET_KEY", default="secret_key")
 ALGORITHM = env.str("ALGORITHM", default="HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = env.int("ACCESS_TOKEN_EXPIRE_MINUTES", default=30)
+
